4f5e688de22ea8e92cfb6c7f1863e277de3c667e
Sasha Patsel <sashapatsel@calvisitor-10-105-161-205.calvisitor.1918.berkeley.edu>
2018-08-09 14:04:44 -0700

    use user input city to query google maps API

--- ajax-2/01/solved/solution.js
+++ ajax-2/01/solved/solution.js
@@ -1,21 +1,33 @@
 
 
-var lat; 
-var long;
-
-var queryURLweather = "https://api.openweathermap.org/data/2.5/weather?q=" + "chicago" + "&APPID=" + "d8d64a31b79e4c92c420bd81c4289876";
-
-$.ajax({
-  url: queryURLweather,
-  method: "GET"
-}).then(function(response) {
-  console.log(response)
-})
-
+var lat = -34.397
+var long = 150.644;
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: lat, lng: long},
     zoom: 8
   });
 }
+
+
+$(".location").on("submit", function(event) {
+  event.preventDefault()
+  var city = $("#location").val()
+
+  var queryURLweather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + "d8d64a31b79e4c92c420bd81c4289876";
+
+  $.ajax({
+    url: queryURLweather,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response)
+    lat = response.coord.lat
+    long = response.coord.lon
+    initMap()
+  })
+
+
+ 
+})
+
