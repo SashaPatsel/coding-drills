22487b2118af5fe6ae841d31f887a2304d037df9
Sasha Patsel <sashapatsel@calvisitor-10-105-161-205.calvisitor.1918.berkeley.edu>
2018-08-09 13:33:11 -0700

    edit ajax for errors

--- ajax-2/01/solved/solution.js
+++ ajax-2/01/solved/solution.js
@@ -1,3 +1,17 @@
+
+
+var lat; 
+var long;
+
+var queryURLweather = "https://api.openweathermap.org/data/2.5/weather?q=" + "chicago" + "&APPID=" + "d8d64a31b79e4c92c420bd81c4289876";
+
+$.ajax({
+  url: queryURLweather,
+  method: "GET"
+}).then(function(response) {
+  console.log(response)
+})
+
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
