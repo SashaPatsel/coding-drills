3906204b6079748f60aba58d48b50cb73d10c9b0
Sasha Patsel <sashapatsel@calvisitor-10-105-161-16.calvisitor.1918.berkeley.edu>
2018-08-10 13:28:11 -0700

    edit Amber's exercise

--- ajax-2/01/solved/solution.js
+++ ajax-2/01/solved/solution.js
@@ -1,33 +1,111 @@
+/* 
+	Before you begin install this chrome extennsion: https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en 
+	make sure the extension is enabled, will be a green icon not red
+*/
 
+/* 
+	add https://funwithajax.herokuapp.com/api/* to intercepted URLs and patterns 
+*/
 
-var lat = -34.397
-var long = 150.644;
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: lat, lng: long},
-    zoom: 8
+/*
+	Create a ready event to make all function available after the document is loaded
+*/
+
+$(document).ready(function () {
+
+  /*///////////// HEROES ACTIVITY ///////////////////*/
+
+  /* Base query url https://funwithajax.herokuapp.com/api/ */
+
+  /* 
+    Create a function call `getHeroes` 
+      Call your function in order to view results 
+  */
+ function getHeroes(){
+
+  var cors = "https://cors-anywhere.herokuapp.com/";
+  var baseURL = "https://funwithajax.herokuapp.com/api";
+  var query = "?q=heroes";
+
+  $.ajax({
+    url: cors+baseURL+query,
+    method: "GET"
+  })
+  .then(function(res){
+
+    console.log("heroes res",res);
+
+    $("body").removeClass("weather-BKG ");
+    $("body").removeClass("music-BKG");
+
+    $("body").addClass("hero-BKG");
+
+    $("#wrap").addClass("blue");
+
+    $("#content").removeClass("music");
+    $("#content").removeClass("weatherWrap");
+
+    $("#content").addClass("heroes");
+
+    $("#content").empty();
+
+    renderHeroesHtml(res);
+
   });
 }
 
+function renderHeroesHtml(res){
 
-$(".location").on("submit", function(event) {
-  event.preventDefault()
-  var city = $("#location").val()
+  var row = $("<div>").addClass("row");
 
-  var queryURLweather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + "d8d64a31b79e4c92c420bd81c4289876";
+  res.forEach(function(squad){
 
-  $.ajax({
-    url: queryURLweather,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response)
-    lat = response.coord.lat
-    long = response.coord.lon
-    initMap()
-  })
+    var col = $("<div>").addClass("col-sm-12 col-md-6 col-lg-6");
+
+    var header = $("<div>").addClass("header1 header");
+    header.html("<h2>"+squad.squadName+"</h2>");
+    $(col).append(header);
+
+    var characters = $("<div>");
+    characters.addClass("characters");
+
+    createHeroesHtml(squad, characters);			
+
+    $(col).append(characters);
+    $(row).append(col);
+
+  });
+
+  $("#content").append(row);
+}
+
+function createHeroesHtml(squad, characters){
+  squad.members.forEach(function(member){
+
+    var memberWrap = $("<div>");
+    memberWrap.addClass("hero");
+
+    var imgWrap = $("<div>").addClass("img-wrap");
+
+    var $image = $("<img>");
+    $image.attr("src", member.image);
+    $image.addClass("hero-image");
+
+    $(imgWrap).append($image);
+
+    var $name = $("<p>");
+    $name.text(member.name);
+
+    var $power = $("<p>");
+    $power.text(member.powers[member.powers.length - 1]);
+
+    $(memberWrap).append(imgWrap, $name, $power);
+    $(characters).append(memberWrap);
+  });
+}
+
+getHeroes();
 
 
- 
-})
 
+});
---
+++ ajax-2/02/readme.md
@@ -0,0 +1,7 @@
+visit [this link](https://cloud.google.com/maps-platform/?__utma=102347093.868891594.1533667148.1533844195.1533844195.1&__utmb=102347093.0.10.1533844195&__utmc=102347093&__utmx=-&__utmz=102347093.1533844195.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)&__utmv=-&__utmk=229294236&_ga=2.208773140.70885818.1533764071-868891594.1533667148#get-started).
+
+- Follow the prompts on the screen. You will eventually be prompted to enter payment info. No need to worry--enter your payment info just to get your API key. You will NOT be charged.
+
+- Follow [this link](https://developers.google.com/maps/documentation/) and select `Maps Javascript API`
+
+- [Enable billing here](https://console.cloud.google.com/google/maps-apis/overview?project=code-drills-1533844217098)
