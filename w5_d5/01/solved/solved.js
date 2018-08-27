08ff2e269060e81d9b1befd76c7a02b767fde440
Sasha Patsel <sashapatsel@calvisitor-10-105-163-53.calvisitor.1918.berkeley.edu>
2018-08-27 10:38:25 -0700

    finish zombie game

--- w5_d5/01/solved/solved.js
+++ w5_d5/01/solved/solved.js
@@ -30,143 +30,144 @@ function damageBool(perc) {
 //POSSIBLE GUNS
 var guns = ["Bear hands", "Machete", "Shotgun", "Pistol", "Machine Gun", "Sniper", "RPG"]
 
-//For 
+//For inquirer prompt
 var zombieChoices = ["zombie1", "zombie2", "zombie3", "zombie4", "zombie5"]
 
+//For objecy lookup
 var zombieIndex = ["zombie1", "zombie2", "zombie3", "zombie4", "zombie5"]
 
 var weapons = {
-bearHands: {
-  //number of zombies affected
-  reach: 1,
-  //Range of possible damage
-  attack: [5,5],
-  //Chance of taking damge back (%)
-  risk: 95,
-  //chance of landing damage (%)
-  reliability: 90,
-  //"health" of gun
-  uses: 1000000000000000 
-},
+  bearHands: {
+    //number of zombies affected
+    reach: 1,
+    //Range of possible damage
+    attack: [5, 5],
+    //Chance of taking damge back (%)
+    risk: 95,
+    //chance of landing damage (%)
+    reliability: 90,
+    //"health" of gun
+    uses: 1000000000000000
+  },
 
-machete: {
-  //number of zombies affected
-  reach: 1,
-  //Range of possible damage
-  attack: [40,60],
-  //Chance of taking damge back (%)
-  risk: 80,
-  //chance of landing damage (%)
-  reliability: 90,
-  //"health" of gun
-  uses: 5 
-},
+  machete: {
+    //number of zombies affected
+    reach: 1,
+    //Range of possible damage
+    attack: [40, 60],
+    //Chance of taking damge back (%)
+    risk: 80,
+    //chance of landing damage (%)
+    reliability: 90,
+    //"health" of gun
+    uses: 5
+  },
 
-shotgun: {
-  //number of zombies affected
-  reach: 2,
-  //Range of possible damage
-  attack: [30,50],
-  //Chance of taking damge back (%)
-  risk: 65,
-  reliability: 80,
-  //"health" of gun
-  uses: 5 
-},
+  shotgun: {
+    //number of zombies affected
+    reach: 2,
+    //Range of possible damage
+    attack: [30, 50],
+    //Chance of taking damge back (%)
+    risk: 65,
+    reliability: 80,
+    //"health" of gun
+    uses: 5
+  },
 
-pistol: {
-  //number of zombies affected
-  reach: 2,
-  //Range of possible damage
-  attack: [25,40],
-  //Chance of taking damge back (%)
-  risk: 50,
-  reliability: 75,
-  //"health" of gun
-  uses: 5 
-},
+  pistol: {
+    //number of zombies affected
+    reach: 2,
+    //Range of possible damage
+    attack: [25, 40],
+    //Chance of taking damge back (%)
+    risk: 50,
+    reliability: 75,
+    //"health" of gun
+    uses: 5
+  },
 
-machineGun: {
-  //number of zombies affected
-  reach: 3,
-  //Range of possible damage
-  attack: [15,30],
-  //Chance of taking damge back (%)
-  risk: 30,
-  reliability: 65,
-  //"health" of gun
-  uses: 5 
-},
+  machineGun: {
+    //number of zombies affected
+    reach: 3,
+    //Range of possible damage
+    attack: [15, 30],
+    //Chance of taking damge back (%)
+    risk: 30,
+    reliability: 65,
+    //"health" of gun
+    uses: 5
+  },
 
-sniper: {
-  //number of zombies affected
-  reach: 1,
-  //Range of possible damage
-  attack: [40,60],
-  //Chance of taking damge back (%)
-  risk: 30,
-  reliability: 10,
-  //"health" of gun
-  uses: 5 
-},
+  sniper: {
+    //number of zombies affected
+    reach: 1,
+    //Range of possible damage
+    attack: [40, 60],
+    //Chance of taking damge back (%)
+    risk: 30,
+    reliability: 10,
+    //"health" of gun
+    uses: 5
+  },
 
-rpg: {
-  //number of zombies affected
-  reach: 4,
-  //Range of possible damage
-  attack: [40,60],
-  //Chance of taking damge back (%)
-  risk: 95,
-  reliability: 40,
-  //"health" of gun
-  uses: 5 
-}
+  rpg: {
+    //number of zombies affected
+    reach: 4,
+    //Range of possible damage
+    attack: [20, 35],
+    //Chance of taking damge back (%)
+    risk: 95,
+    reliability: 40,
+    //"health" of gun
+    uses: 5
+  }
 }
 
 var zombies = {
 
-     zombie1: { 
-      health: 100,
-      attack: [0, 5],
-      //Chance of dodging (%)
-      agility: 15,
-      receivedDam: false
-    },
+  zombie1: {
+    health: 100,
+    attack: [0, 5],
+    //Chance of dodging (%)
+    agility: 15,
+    receivedDam: false
+  },
 
-     zombie2: { 
-      health: 90,
-      attack: [1, 6],
-      //Chance of dodging (%)
-      agility: 18,
-      receivedDam: false
-    },
+  zombie2: {
+    health: 90,
+    attack: [1, 6],
+    //Chance of dodging (%)
+    agility: 18,
+    receivedDam: false
+  },
 
-     zombie3: { 
-      health: 85,
-      attack: [2, 4],
-      //Chance of dodging (%)
-      agility: 20,
-      receivedDam: false
-    },
-
-
-     zombie4: { 
-      health: 80,
-      attack: [2, 5],
-      //Chance of dodging (%)
-      agility: 22,
-      receivedDam: false
-    },
+  zombie3: {
+    health: 85,
+    attack: [2, 4],
+    //Chance of dodging (%)
+    agility: 20,
+    receivedDam: false
+  },
 
 
-     zombie5: { 
-      health: 60,
-      attack: [7, 10],
-      //Chance of dodging (%)
-      agility: 10,
-      receivedDam: false
-    }
+  zombie4: {
+    health: 80,
+    attack: [2, 5],
+    //Chance of dodging (%)
+    agility: 22,
+    receivedDam: false
+  },
+
+
+  zombie5: {
+    health: 60,
+    attack: [7, 10],
+    //Chance of dodging (%)
+    agility: 10,
+    receivedDam: false
   }
+}
 
 
 // Created a generic function that checks if the user won or lost.
@@ -212,19 +213,19 @@ function checkRound() {
 // This function holds the game logic
 function playRound() {
 
-  for (var i = 0 ; i < zombieIndex.length ; i++) {
+  for (var i = 0; i < zombieIndex.length; i++) {
     // Pass in the names of the zombies in the array to locate them in the object.
     var zIndex = "zombie" + (i + 1)
 
     if (zombies[zIndex].health > 0) {
       console.log("\n=============================\n")
-      console.log(zombieIndex[i] +" Health: ", zombies[zIndex].health)
+      console.log(zombieIndex[i] + " Health: ", zombies[zIndex].health)
 
     }
   }
   console.log("\n=============================\n")
   console.log("Hero Health: " + hero.health + "\n")
- 
+
   // We create a list prompt. Specifying that the user must pick a random number between 1 and 5.
   inquirer.prompt([
     {
@@ -234,15 +235,15 @@ function playRound() {
       choices: guns
     }
 
-  ]).then(function(game) {
-    console.log("You chose a "+ game.gun + "!")
+  ]).then(function (game) {
+    console.log("You chose a " + game.gun + "!")
     switch (game.gun) {
 
       case "Bear hands":
         //Set the player's damae to the range
         playerDam = calcDamage(weapons.bearHands.attack[0], weapons.bearHands.attack[1]);
         playerDidDam = damageBool(weapons.bearHands.reliability)
-        
+
         weapons.bearHands.uses--
 
         pickZombie(weapons.bearHands)
@@ -278,7 +279,7 @@ function playRound() {
         pickZombie(weapons.sniper)
         break;
 
-      case "RPG": 
+      case "RPG":
         playerDam = calcDamage(weapons.rpg.attack[0], weapons.rpg.attack[1]);
         playerDidDam = damageBool(weapons.rpg.reliability)
         weapons.rpg.uses--
@@ -299,41 +300,61 @@ function pickZombie(weapon) {
       choices: zombieChoices
     }
 
-  ]).then(function(zombie) {
-    zombies[zombie.zombie].receivedDam = damageBool(zombies[zombie.zombie].agility)
+  ]).then(function (zombie) {
+
+    // Determine reach
+    var zombieVictims = []
+    zombieVictims.push(zombie.zombie)
+    for (var i = 0; i < zombieChoices.length; i++) {
+      if (zombieVictims.length < weapon.reach && zombieChoices[i] !== zombie.zombie) {
+        zombieVictims.push(zombieChoices[i])
+      }
+    }
 
     if (!damageBool(weapon.reliability)) {
       console.log("Shot on target")
-    
-    if (zombies[zombie.zombie].receivedDam === true ) {
-      console.log("Congrats, you landed a shot for " + playerDam + " damage on " + zombie.zombie +"!")
-      zombies[zombie.zombie].health -= playerDam
-      
-    } else {
-      console.log("The zombie dodged your attack")
-    }
 
-  } else {
-    console.log("Oooooh, tough miss")
-  }
+      for (var i = 0; i < zombieVictims.length; i++) {
+        zombies[zombieVictims[i]].receivedDam = damageBool(zombies[zombieVictims[i]].agility)
 
-    if (zombies[zombie.zombie].health <= 0) {
 
-      for (var i = 0 ; i < zombieChoices.length ; i++) {
-        if (zombieChoices[i] === zombie.zombie) {
-          console.log("Congrats, you defeated " + zombieChoices[i])
-          //Make the zombie unable to attack anymore
-          zombies[zombie.zombie].attack = [0,0]
-          zombieChoices.splice(i,1) 
+
+        if (zombies[zombieVictims[i]].receivedDam === true) {
+          console.log("Congrats, you landed a shot for " + playerDam + " damage on " + zombieVictims[i] + "!")
+          zombies[zombieVictims[i]].health -= playerDam
+
+        } else {
+          console.log(zombieVictims[i] + " dodged your attack")
         }
+
+
+        //If a zombie is defeated
+        if (zombies[zombieVictims[i]].health <= 0) {
+
+          for (var i = 0; i < zombieChoices.length; i++) {
+            if (zombieChoices[i] === zombieVictims[i]) {
+              console.log("Congrats, you defeated " + zombieChoices[i])
+              //Make the zombie unable to attack anymore
+              zombies[zombieVictims[i]].attack = [0, 0]
+              //Remove the zombie as a choice in the inquirer prompt
+              zombieChoices.splice(i, 1)
+            }
+          }
+
+        }
+
       }
-      
+    } else {
+      console.log("Oooooh, tough miss. Your shot was not on target.")
     }
 
+    
+
+    // If the hero takes damage
     if (!damageBool(weapon.risk)) {
-      var zombieDamSum = calcDamage(zombies.zombie1.attack[0],[1]) + calcDamage(zombies.zombie2.attack[0],[1]) + calcDamage(zombies.zombie3.attack[0],[1]) + calcDamage(zombies.zombie4.attack[0],[1]) + calcDamage(zombies.zombie5.attack[0],[1]);
-      
-      hero.health-= zombieDamSum
+      var zombieDamSum = calcDamage(zombies.zombie1.attack[0], [1]) + calcDamage(zombies.zombie2.attack[0], [1]) + calcDamage(zombies.zombie3.attack[0], [1]) + calcDamage(zombies.zombie4.attack[0], [1]) + calcDamage(zombies.zombie5.attack[0], [1]);
+
+      hero.health -= zombieDamSum
       console.log("The hero has been dealt " + zombieDamSum + " damage.")
       console.log("The hero has " + hero.health + " health.")
 
@@ -341,7 +362,7 @@ function pickZombie(weapon) {
       console.log("Phew, that was a close one")
     }
     checkRound()
-  })  
+  })
 }
 
 
