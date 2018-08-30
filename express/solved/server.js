7d5fade57caaf2713538068bb4b624a417529319
Sasha Patsel <sashapatsel@calvisitor-10-105-161-136.calvisitor.1918.berkeley.edu>
2018-08-30 12:48:14 -0700

    get response from server on front end (express)

--- express/solved/server.js
+++ express/solved/server.js
@@ -6,9 +6,26 @@ var bodyParser = require("body-parser")
 var request = require("request")
 // TO use HTMl files
 var path = require("path")
+// To query our db
+var mysql = require("mysql");
 
 var app = express()
 var PORT = 3002
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  user: "root",
+
+  password: "",
+  database: "restaurantsDB"
+
+})
+
+connection.connect(function(err) {
+  if (err) throw err
+})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -19,6 +36,16 @@ app.use(express.static("public"));
  app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"))
  })
+
+ app.get("/api/restaurants", function(req, res) {
+
+
+  connection.query("SELECT * FROM restaurants", function(err, response) {
+    return res.json(response)
+  })
+
+ })
+
 
 
 
