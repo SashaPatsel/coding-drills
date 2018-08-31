d3c8c263f0cb33072415dce4076009d56ee1b050
Sasha Patsel <sashapatsel@calvisitor-10-105-162-120.calvisitor.1918.berkeley.edu>
2018-08-31 10:39:50 -0700

    stars each have unique behavior

--- express/solved/public/js/stars.js
+++ express/solved/public/js/stars.js
@@ -2,39 +2,43 @@
 
 $(document).on("mouseenter", ".stars1", function() {
         
-  $(".stars1").addClass("star-hov")
+
+  $(this).addClass("star-hov")
 
 })
 
 $(document).on("mouseenter", ".stars2", function() {
         
-  $(".stars1").addClass("star-hov")
-  $(".stars2").addClass("star-hov")
+
+  $(this).prev().addClass("star-hov")
+  $(this).addClass("star-hov")
 })
 
 
 $(document).on("mouseenter", ".stars3", function() {
   
-  $(".stars1").addClass("star-hov")
-  $(".stars2").addClass("star-hov")
-  $(".stars3").addClass("star-hov")
+
+  $(this).prev().prev().addClass("star-hov")
+  $(this).prev().addClass("star-hov")
+  $(this).addClass("star-hov")
 })
 
 $(document).on("mouseenter", ".stars4", function() {
   
-  $(".stars1").addClass("star-hov")
-  $(".stars2").addClass("star-hov")
-  $(".stars3").addClass("star-hov")
-  $(".stars4").addClass("star-hov")
+
+  $(this).prev().prev().prev().addClass("star-hov")
+  $(this).prev().prev().addClass("star-hov")
+  $(this).prev().addClass("star-hov")
+  $(this).addClass("star-hov")
 })
 
 $(document).on("mouseenter", ".stars5", function() {
-  
-  $(".stars1").addClass("star-hov")
-  $(".stars2").addClass("star-hov")
-  $(".stars3").addClass("star-hov")
-  $(".stars4").addClass("star-hov")
-  $(".stars5").addClass("star-hov")
+  $(this).prev().prev().prev().prev().addClass("star-hov")
+  $(this).prev().prev().prev().addClass("star-hov")
+  $(this).prev().prev().addClass("star-hov")
+  $(this).prev().addClass("star-hov")
+  $(this).addClass("star-hov")
+
 
 })
 
@@ -42,56 +46,63 @@ $(document).on("mouseenter", ".stars5", function() {
 
 $(document).on("click", ".stars1", function() {
         
-  $(".stars1").addClass("star-click")
-
-  $(".stars2").removeClass("star-click")
-  $(".stars3").removeClass("star-click")
-  $(".stars4").removeClass("star-click")
-  $(".stars5").removeClass("star-click")
+  $(this).addClass("star-click")
+  
+ 
+  $(this).next().next().next().next().removeClass("star-click")
+  $(this).next().next().next().removeClass("star-click")
+  $(this).next().next().removeClass("star-click")
+  $(this).next().removeClass("star-click")
 
 })
 
 $(document).on("click",".stars2", function() {
         
-  $(".stars1").addClass("star-click")
-  $(".stars2").addClass("star-click")
+  
+  
+  $(this).prev().addClass("star-click")
+  
+  $(this).addClass("star-click")
 
-  $(".stars3").removeClass("star-click")
-  $(".stars4").removeClass("star-click")
-  $(".stars5").removeClass("star-click")
+  $(this).next().next().next().removeClass("star-click")
+  $(this).next().next().removeClass("star-click")
+  $(this).next().removeClass("star-click")
 })
 
 
 $(document).on("click",".stars3", function() {
   
-  $(".stars1").addClass("star-click")
-  $(".stars2").addClass("star-click")
-  $(".stars3").addClass("star-click")
+  $(this).prev().prev().addClass("star-click")
+  $(this).prev().addClass("star-click")
+  
+  $(this).addClass("star-click")
 
-  $(".stars4").removeClass("star-click")
-  $(".stars5").removeClass("star-click")
+  $(this).next().next().removeClass("star-click")
+  $(this).next().removeClass("star-click")
 
 })
 
 $(document).on("click", ".stars4",function() {
   
-  $(".stars1").addClass("star-click")
-  $(".stars2").addClass("star-click")
-  $(".stars3").addClass("star-click")
-  $(".stars4").addClass("star-click")
+  $(this).prev().prev().prev().addClass("star-click")
+  $(this).prev().prev().addClass("star-click")
+  $(this).prev().addClass("star-click")
+  
+  $(this).addClass("star-click")
 
 
-  $(".stars5").removeClass("star-click")
+  $(this).next().removeClass("star-click")
 
 })
 
 $(document).on("click", ".stars5",function() {
   
-  $(".stars1").addClass("star-click")
-  $(".stars2").addClass("star-click")
-  $(".stars3").addClass("star-click")
-  $(".stars4").addClass("star-click")
-  $(".stars5").addClass("star-click")
+
+  $(this).prev().prev().prev().prev().addClass("star-click")
+  $(this).prev().prev().prev().addClass("star-click")
+  $(this).prev().prev().addClass("star-click")
+  $(this).prev().addClass("star-click")
+  $(this).addClass("star-click")
 
 
 
@@ -100,42 +111,42 @@ $(document).on("click", ".stars5",function() {
 //========== MOUSE LEAVE ============
 
 $(document).on("mouseleave", ".stars5",function() {
-  $(".stars1").removeClass("star-hov")
-  $(".stars2").removeClass("star-hov")
-  $(".stars3").removeClass("star-hov")
-  $(".stars4").removeClass("star-hov")
-  $(".stars5").removeClass("star-hov")
+  $(this).prev().prev().prev().prev().removeClass("star-hov")
+  $(this).prev().prev().prev().removeClass("star-hov")
+  $(this).prev().prev().removeClass("star-hov")
+  $(this).prev().removeClass("star-hov")
+  $(this).removeClass("star-hov")
 })
 
 $(document).on("mouseleave", ".stars4",function() {
-  $(".stars1").removeClass("star-hov")
-  $(".stars2").removeClass("star-hov")
-  $(".stars3").removeClass("star-hov")
-  $(".stars4").removeClass("star-hov")
-  $(".stars5").removeClass("star-hov")
+  $(this).prev().prev().prev().prev().removeClass("star-hov")
+  $(this).prev().prev().prev().removeClass("star-hov")
+  $(this).prev().prev().removeClass("star-hov")
+  $(this).prev().removeClass("star-hov")
+  $(this).removeClass("star-hov")
 })
 
 $(document).on("mouseleave", ".stars3",function() {
-  $(".stars1").removeClass("star-hov")
-  $(".stars2").removeClass("star-hov")
-  $(".stars3").removeClass("star-hov")
-  $(".stars4").removeClass("star-hov")
-  $(".stars5").removeClass("star-hov")
+  $(this).prev().prev().prev().prev().removeClass("star-hov")
+  $(this).prev().prev().prev().removeClass("star-hov")
+  $(this).prev().prev().removeClass("star-hov")
+  $(this).prev().removeClass("star-hov")
+  $(this).removeClass("star-hov")
 })
 
 $(document).on("mouseleave", ".stars2",function() {
-  $(".stars1").removeClass("star-hov")
-  $(".stars2").removeClass("star-hov")
-  $(".stars3").removeClass("star-hov")
-  $(".stars4").removeClass("star-hov")
-  $(".stars5").removeClass("star-hov")
+  $(this).prev().prev().prev().prev().removeClass("star-hov")
+  $(this).prev().prev().prev().removeClass("star-hov")
+  $(this).prev().prev().removeClass("star-hov")
+  $(this).prev().removeClass("star-hov")
+  $(this).removeClass("star-hov")
 })
 
 $(document).on("mouseleave", ".stars1",function() {
-  $(".stars1").removeClass("star-hov")
-  $(".stars2").removeClass("star-hov")
-  $(".stars3").removeClass("star-hov")
-  $(".stars4").removeClass("star-hov")
-  $(".stars5").removeClass("star-hov")
+  $(this).prev().prev().prev().prev().removeClass("star-hov")
+  $(this).prev().prev().prev().removeClass("star-hov")
+  $(this).prev().prev().removeClass("star-hov")
+  $(this).prev().removeClass("star-hov")
+  $(this).removeClass("star-hov")
 })
 
