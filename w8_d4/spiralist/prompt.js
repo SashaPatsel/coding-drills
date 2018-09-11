e72b016f93e3aadbc47423baf411829fdd0db63d
Sasha Patsel <sashapatsel@calvisitor-10-105-161-178.calvisitor.1918.berkeley.edu>
2018-09-11 12:54:55 -0700

    halfway through spiral iteration instructions

--- w8_d4/spiralist/prompt.js
+++ w8_d4/spiralist/prompt.js
@@ -1,6 +1,22 @@
 // Follow the pseudocode step-by-step as detailed below. It may help to think of this problem with a specific example in mind. Try imagining that we are given the number 3.
 function spiralist(n) {
-  //1. Create an empty array that contains all the sub arrays. This is what we will ultimately return.
+  // 1. Create an empty array that contains all the sub arrays. This is what we will ultimately return. For the sake of this pseudocode, let's call it "final".
+
+  // 2. We will call the given number n. Push n arrays into final.
+  
+  // 3. Create a variable. Let's call it counter. This will represent the numbers we send to the matrix. Set counter to 1, since 1 is always at final[0][0].
+
+  // 4. Create 4 new variables. One of the key parts of this problem is identifying where we currently are in our iteration. Using these helper variables will help us narrow the scope of where we can insert numbers.
+  //    a. A variable to keep track of the starting column
+  //    b. A variable to keep track of the ending column
+  //    c. A variable to keep track of the starting row
+  //    d. A variable to keep track of the ending row
+  // Try to think what the above variables should be set to. Remember, these will be used to tell us where we're inserting our counter value. 
+  
+
+  // 5. Almost all of the rest of your logic should execute under the following condition: the start column is less than or equal to the end column AND the start row is less than or equal to the end row.
+
+  // 6. Create 
 
 }
 
