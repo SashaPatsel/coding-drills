de11799098c1f2c8e0f85b3ad9bd214ed7c3465e
Sasha Patsel <sashapatsel@calvisitor-10-105-161-66.calvisitor.1918.berkeley.edu>
2018-09-12 11:39:36 -0700

    solved the SKYLINE muthaf***agit add .

--- w8_d4/sf-mayor/solved/solved.js
+++ w8_d4/sf-mayor/solved/solved.js
@@ -10,6 +10,8 @@ function skyline(arr) {
   var holderX = []
   var holderY = []
 
+  var sum = 0;
+
   for (var i = 0 ; i < arr.length ; i++) {
     
     var highestXInfo = []
@@ -44,7 +46,44 @@ function skyline(arr) {
 
   console.log("X",holderX)
   console.log("Y",holderY)
-  
+
+  for (var i = 0 ; i < arr.length ; i ++) {
+
+    for (var j = 0 ; j < arr.length ; j ++) {
+      
+      var tallest = [arr[i][j]]
+      // After getting the tallest for each row and column, we can now compare each element against those values. 
+      for (var x = 0 ; x < holderX.length ; x++) {
+        for (var y = 0 ; y < holderX.length ; y++) {
+
+          if (i === holderX[x][1] && j === holderY[y][1]) {
+          
+            tallest.push(holderX[x][0], holderY[y][0])
+
+          }
+        }
+      }
+
+
+
+      if (tallest.length === 3 && tallest[0] < tallest[1] && tallest[0] < tallest[2]) {
+        console.log("tallest", tallest)
+        var smaller = tallest[1]
+        
+        if (tallest[2] <= smaller) {
+          smaller = tallest[2]
+        }
+        
+        var newHeight = smaller - 1
+        
+        console.log("diff", newHeight, arr[i][j])
+        sum += (smaller - arr[i][j] )
+      }
+
+    }
+
+  }
+  return sum
 
 }
 
