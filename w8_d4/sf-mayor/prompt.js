08c1e2cf3d007c1c304e7a035f30978bb74f5180
Sasha Patsel <sashapatsel@calvisitor-10-105-161-66.calvisitor.1918.berkeley.edu>
2018-09-12 14:08:30 -0700

    add pseudocode to prompt

--- w8_d4/sf-mayor/prompt.js
+++ w8_d4/sf-mayor/prompt.js
@@ -17,9 +17,20 @@ var input2 = [
 
 function skyline(arr) {
 // STEPS TO SOLVING THIS PROBLEM:
-// 1. Find the highest building in each row and in each column.
+// 1. Create a variable that keeps track of the sum of the buildings' increase in height. We won't be using this variable for another few steps.
+// 2. Find the highest building in each row and in each column.
 //    a. HINT: Keep in mind this is a SQUARE matrix. 
-//    b. 
+//    b. Create two empty arrays, one containing all the highest values in a row and which row that highest value is in, and another doing the same but for columns.
+//    c. CHALLENGE: Send the highest values in a row and which row that highest value is in, and the same for columns, to the containers you made in 2.b.
+//    d. If the matrix is a 4x4, each of your containers should have four arrays within, each array containing the value of the highest building, as well as the index of its row/column
+
+// 3. Now we've set up the information in a way that lets us do the math this problem asks for. We'll need to iterate through every value in the matrix again, but this time we can COMPARE EACH value to the tallest in its respective row and column.
+//    a. You may need a number of loops to solve this
+//    b. Store the value your looking at as well as its tallest row and tallest column building in an array.
+
+// 4. For each element in the matrix, compare the two values of the tallest in the row, and the tallest in the column. You should add the difference between the height of the shorter one, minus one, and the elements actual size to the final sum.
+
+// 5. Return your tracking variable.
 }
 
 
