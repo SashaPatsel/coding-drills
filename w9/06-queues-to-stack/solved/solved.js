cf9e25d02e6c8b536e9582a0341df8d2da68eff4
Sasha Patsel <sashapatsel@calvisitor-10-105-186-114.calvisitor.1918.berkeley.edu>
2018-09-18 11:11:30 -0700

    solve stack from queues exercise

--- w9/06-queues-to-stack/solved/solved.js
+++ w9/06-queues-to-stack/solved/solved.js
@@ -18,20 +18,36 @@ var Queue = function(data = []) {
 
 
 var Stack = function() {
+  // Unlike the queue from two stack exercise, we only need to instantiate one queue for now.
   this.q1 = new Queue([1,2,3,4,5,6,7])
-  this.q2 = new Queue([])
+  
 
   this.add = function(record) {
-    while (this.q1.peek()) {
-      this.q2
-    }
+  //Instantiate empty queue here.  
+  var q2 = new Queue([])    
+  // Add the new entry to the front of the new queue
+  q2.add(record)
+  // Then add all the entries from the populated queue after the new entry
+  while (this.q1.peek()) {
+    q2.add(this.q1.remove())
+  }
+  // q2 is essentially an updated version of q1, so we can assign q1 the value of q2
+  this.q1 = q2
   }
 
   this.remove = function() {
-
+    //  This works because of the way we configured add
+    return this.q1.remove()
   }
 
   this.peek = function() {
-
+    // Just look at the first entry in the queue.
+    return this.q1.data[0]
   }
 }
+
+var s = new Stack()
+
+console.log(s.peek())
+s.add(8)
+console.log(s.peek())
