a3251b3f1f6e52dcd5afb9aab2c1f379254ade44
Sasha Patsel <sashapatsel@calvisitor-10-105-186-114.calvisitor.1918.berkeley.edu>
2018-09-18 10:42:35 -0700

    fix errors in queue from stacks

--- w9/05-stacks-to-queue/solved/solved.js
+++ w9/05-stacks-to-queue/solved/solved.js
@@ -22,22 +22,22 @@ var Queue = function() {
   this.stack1 = new Stack([1,2,3,4,5,6,7])
   this.stack2 = new Stack([])
   // Nothing too special here, just adding an entry to the top of our "first pile of books".
-  this.add = function() {
-    this.stack1.push(record)
+  this.add = function(record) {
+    this.stack1.add(record)
   }
 
 
   this.remove = function() {
     // Move all the entries to the empty stack. Stop when there's nothing when we "peek" in the first stack
     while (this.stack1.peek()) {
-      this.stack2.push(this.stack1.pop())
+      this.stack2.add(this.stack1.remove())
     }
     // Store the top value on the second stack
-    var record = this.stack2.pop()
+    var record = this.stack2.remove()
 
     // Put the elements back in the first stack. Do so until there are no more elements in the second stack.
     while (this.stack2.peek()) {
-      this.stack1.push(this.stack2.pop())
+      this.stack1.add(this.stack2.remove())
     }
 
     return record
@@ -47,13 +47,13 @@ var Queue = function() {
   this.peek = function() {
     // Same thing as above, except we're just looking rather than removing
     while (this.stack1.peek()) {
-      this.stack2.push(this.stack1.pop())
+      this.stack2.add(this.stack1.remove())
     }
 
     var record = this.stack2.peek()
 
     while (this.stack2.peek()) {
-      this.stack1.push(this.stack2.pop())
+      this.stack1.add(this.stack2.remove())
     }
 
     return record
@@ -62,4 +62,21 @@ var Queue = function() {
 }
 
 
+var q = new Queue()
 
+console.log(q.peek())
+
+
+console.log(q.remove())
+console.log(q.remove())
+console.log(q.remove())
+console.log(q.remove())
+console.log(q.remove())
+console.log(q.remove())
+console.log(q.remove())
+
+q.add(11)
+q.add(14)
+q.add(3)
+q.add(7)
+console.log(q.peek())
--- w9/06-queues-to-stack/solved/solved.js
+++ w9/06-queues-to-stack/solved/solved.js
@@ -18,5 +18,20 @@ var Queue = function(data = []) {
 
 
 var Stack = function() {
-  
+  this.q1 = new Queue([1,2,3,4,5,6,7])
+  this.q2 = new Queue([])
+
+  this.add = function(record) {
+    while (this.q1.peek()) {
+      this.q2
+    }
+  }
+
+  this.remove = function() {
+
+  }
+
+  this.peek = function() {
+
+  }
 }
