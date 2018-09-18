04a7161f20e1a974f72c8af0cc3dca8f981fcbe1
Sasha Patsel <sashapatsel@calvisitor-10-105-186-114.calvisitor.1918.berkeley.edu>
2018-09-18 10:22:42 -0700

    add hint for stacks to queue

--- w9/05-stacks-to-queue/solved/solved.js
+++ w9/05-stacks-to-queue/solved/solved.js
@@ -14,13 +14,7 @@ var Stack = function(data = []) {
     return this.data[data.length -1]
   }
 }
-// This is a very inefficient way to create a Queue, which is why this exercise may be challenging...
 
-// It may help to think of these as stacks of books. A stack of books is, well... a stack. When we pile one book on top of another, the first book we can take is the last one we placed. If we don't take the top book out first, the pile topples. 
-
-// The way we want the stack of books to behave is like a queue. We want to be able to get the bottom book out. This is where the second stack comes in. Imagine we have a stack of three books, and an empty pile. To get the book at the bottom--in other words, to mimick the behavior of a queue--we need to take the top book from our first pile, and move it to our empty pile. Then we move the next book, and finally the last. We've just turned our stack upside down, which is sort of a queue. To get the value we're looking for, we just take the top book from the stack we just created, which was the book at the bottom of the other stack. Finally, we need to reverse the process and put all the remaining books back so that our queue retains FIFO behavior. 
-
-// Obviously, this is an incredibly inefficient way of making a queue, but being able to do this demonstartes your understanding of both data structures. This is somewhat like the Hanoi Towers (look up the exercise on Google if you are unfamiliar)
 
 var Queue = function() {
 
@@ -41,7 +35,7 @@ var Queue = function() {
     // Store the top value on the second stack
     var record = this.stack2.pop()
 
-    // Put the elements back in the first stack
+    // Put the elements back in the first stack. Do so until there are no more elements in the second stack.
     while (this.stack2.peek()) {
       this.stack1.push(this.stack2.pop())
     }
