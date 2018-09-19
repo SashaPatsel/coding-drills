009f2e80ff5336fc5f2a7157e5ded0d656c7bf71
Sasha Patsel <sashapatsel@calvisitor-10-105-184-108.calvisitor.1918.berkeley.edu>
2018-09-19 12:29:05 -0700

    add comments for solved in 04

--- w9-d3/04-branch-width/solved/solved.js
+++ w9-d3/04-branch-width/solved/solved.js
@@ -1,24 +1,3 @@
-function branchWidth(root) {
-  var arr = [root, "stop"]
-
-  var widths = [0]
-
-  while(arr.length > 1) {
-    var node = arr.shift()
-    if(node === "stop") {
-      widths.push(0)
-      arr.push("stop")
-    } else {
-      for (var i = 0 ; i < node.children.length ; i++) {
-        arr.push(node.children[i]) 
-      }
-      widths[widths.length -1] ++
-    }
-  }
-
-  return widths
-}
-
 var Node = function (data) {
   this.data = data
   this.children = []
@@ -34,6 +13,40 @@ var Node = function (data) {
     })
   }
 }
+
+
+
+function branchWidth(root) {
+  // Initialize an array with the root node and a string we can use later as a condition check
+  var arr = [root, "stop"]
+// What we'll return
+  var widths = [0]
+
+  while(arr.length > 1) {
+    // Grab the first value in arr
+    var node = arr.shift()
+    // If Stop is the first value in arr
+    if(node === "stop") {
+      // re-initialize widths with 0
+      widths.push(0)
+      // Add another stop for condition checks
+      arr.push("stop")
+      // When the first value in arr is NOT "stop"
+    } else {
+
+      for (var i = 0 ; i < node.children.length ; i++) {
+        // Send every child node to arr
+        arr.push(node.children[i]) 
+      }
+      // Increment the lates value we're working on in widths. This stops getting added to when we hit "stop"
+      widths[widths.length -1] ++
+    }
+  }
+
+  return widths
+}
+
+
 
 var root = new Node(0);
 root.add(1);
