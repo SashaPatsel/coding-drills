d872ed12e3094aebecccd16e10bbf51c8b77c4f7
Sasha Patsel <sashapatsel@calvisitor-10-105-184-108.calvisitor.1918.berkeley.edu>
2018-09-19 10:49:16 -0700

    finish 02

--- w9-d3/02-tree-builder-pt.2/hint/hint.js
+++ w9-d3/02-tree-builder-pt.2/hint/hint.js
@@ -18,9 +18,23 @@ var Node = function (data) {
 var Tree = function () {
   this.root;
 
-  this.breadth = function (func) {
+  // There are a lot of ways to think about this problem. Rememeber, we want to iterate through all of the children of a given node, and only when all of those nodes have been iterated through do we want to move on to the grandchildren.
 
+  this.breadth = function (func) {
+    // Initialize an array which contains only this.root
+
+    // Create a loop that lasts as long as the array we just created has a length greater than 0.
+
+      // create a variable that returns and removes the first element of the array we created.
+
+      // Create a loop that iterates through all  the children of the variable we created in the last step
+
+      // Send each child to to the array we created.
+
+      // Invoke the callback "func" on each node (what the variable we created points to)
   }
+
+
 }
 
 
