d0b41409c8d0b29d5458abe3c161f1d5ac1d2a8e
Sasha Patsel <sashapatsel@Sashas-MacBook-Pro.local>
2018-09-24 12:35:22 -0700

    solve 05 w10-d1

--- w10-d1/05-back-n-forth-history/solved/solved.js
+++ w10-d1/05-back-n-forth-history/solved/solved.js
@@ -5,20 +5,20 @@ var path = require("path");
 var app = express();
 var PORT = 2018;
 
-var Stack = function(data = []) {
- 
+var Stack = function (data = []) {
+
   this.data = data
 
-  this.add = function(record) {
+  this.add = function (record) {
     this.data.push(record)
   }
 
-  this.remove = function() {
+  this.remove = function () {
     return this.data.pop()
   }
 
-  this.peek = function() {
-    return this.data[data.length -1]
+  this.peek = function () {
+    return this.data[data.length - 1]
   }
 
 }
@@ -29,73 +29,68 @@ var forwardHistory = new Stack()
 
 // API ROUTES
 
-app.post("/new", function(req, res) {
-  res.redirect()
+app.get("/back", function (req, res) {
+  if (backHistory.peek()) {
+    // Add the current page to the forwards history
+    forwardHistory.add(backHistory.remove())
+
+    return res.redirect(backHistory.remove())
+  }
+
 })
 
-app.get("/back", function(req, res) {
-  console.log("histor",backHistory.remove())
-  res.redirect("http://localhost:2018/rhino")
-})
+app.get("/forward", function (req, res) {
+  if (forwardHistory.peek()) {
+    return res.redirect(forwardHistory.remove())
+  }
 
-app.get("/forward", function(req, res) {
-  forwardHistory.add()
 })
 
 
 
 
 // HTML ROUTES
-app.get("/", function(req, res) {
-
-  console.log((`${req.originalUrl}`))
+app.get("/", function (req, res) {
   backHistory.add(`${req.originalUrl}`)
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/rhino", function(req, res) {
-  console.log((`${req.originalUrl}`))
+app.get("/rhino", function (req, res) {
   backHistory.add(`${req.originalUrl}`)
   res.sendFile(path.join(__dirname, "rhino.html"));
 });
 
-app.get("/gorilla", function(req, res) {
-  console.log((`${req.originalUrl}`))
+app.get("/gorilla", function (req, res) {
   backHistory.add(`${req.originalUrl}`)
   res.sendFile(path.join(__dirname, "gorilla.html"));
 });
 
-app.get("/tiger", function(req, res) {
-  console.log((`${req.originalUrl}`))
+app.get("/tiger", function (req, res) {
   backHistory.add(`${req.originalUrl}`)
   res.sendFile(path.join(__dirname, "tiger.html"));
 });
 
-app.get("/eagle", function(req, res) {
-  console.log((`${req.originalUrl}`))
+app.get("/eagle", function (req, res) {
   backHistory.add(`${req.originalUrl}`)
   res.sendFile(path.join(__dirname, "eagle.html"));
 });
 
-app.get("/turtle", function(req, res) {
-  console.log((`${req.originalUrl}`))
+app.get("/turtle", function (req, res) {
   backHistory.add(`${req.originalUrl}`)
   res.sendFile(path.join(__dirname, "turtle.html"));
 });
 
-app.get("/buffalo", function(req, res) {
-  console.log((`${req.originalUrl}`))
+app.get("/buffalo", function (req, res) {
   backHistory.add(`${req.originalUrl}`)
   res.sendFile(path.join(__dirname, "buffalo.html"));
 });
 
-app.get("/hippo", function(req, res) {
-  console.log((`${req.originalUrl}`))
+app.get("/hippo", function (req, res) {
   backHistory.add(`${req.originalUrl}`)
   res.sendFile(path.join(__dirname, "hippo.html"));
 });
 
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
