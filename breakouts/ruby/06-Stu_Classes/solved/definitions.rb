25d9b605ab560d15780691b765834880236baf83
Sasha Patsel <user@Users-MacBook-Pro.local>
2019-01-14 09:58:39 -0800

    add store and item classes to ruby breakout

--- breakouts/ruby/06-Stu_Classes/solved/definitions.rb
+++ breakouts/ruby/06-Stu_Classes/solved/definitions.rb
@@ -8,14 +8,41 @@ class Store
         @items = items
     end
 
+    def print_bio
+        puts "Store store name: #{@store_name}"
+        puts "Store category: #{@category}"
+        puts "Store location: #{@location}"
+    end
+
     def add_items(item)
         @items += item
     end
 
     def remove_items(item)
         @items -= item
-    end    
+    end   
 
+end
 
+class item
+    attr_accessor :item_name, :price, :stock
+
+    def initialize(item_name, price, stock)
+        @item_name = item_name
+        @price = price
+        @stock = stock
+    end
+
+    def set_price(new_price)
+        @price = new_price
+    end
+
+    def less_stock(amount)
+        @stock -= amount
+    end
+
+    def add_stock(amount)
+        @stock += amount
+    end
 
 end
---
+++ breakouts/ruby/06-Stu_Classes/solved/solved.rb
@@ -0,0 +1,19 @@
+require "./definitions"
+
+safeway_items = [
+    {
+        :name => "hummus",
+        :price => 4.50
+    },
+    {
+        :name => "cream cheese",
+        :price => 2.50
+    },
+    {
+        :name => "guacamole",
+        :price => 
+    }
+
+]
+
+safeway = Store.new "Safeway", "San Francisco", "Grocery", safeway_items
