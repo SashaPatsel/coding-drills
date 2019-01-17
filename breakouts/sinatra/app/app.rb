7d6e788017535fe89942642f29a6b2fc653cf004
Sasha Patsel <user@Users-MacBook-Pro.local>
2019-01-17 10:54:45 -0800

    query mysql db with sinatra

--- breakouts/sinatra/app/app.rb
+++ breakouts/sinatra/app/app.rb
@@ -1,7 +1,20 @@
+
+require 'nokogiri'
 require 'rest-client'
 require "sinatra"
-# require "rest-client"
+require "mysql2"
 
+
+Mysqlclient = Mysql2::Client.new(host: 'localhost',
+                                 username: 'root',
+                                 password: 'password',
+                                 database: 'cat_db')
+
+def get_cats
+  Mysqlclient.query('SELECT * FROM cats', :as => :hash).to_a
+end
+
+puts get_cats
 
 class HiSinatra < Sinatra::Base
     get "/" do
@@ -15,12 +28,18 @@ class HiSinatra < Sinatra::Base
 
     get "/api" do
         data = RestClient.get('https://api.unsplash.com//photos/random?client_id=07dacab89ab64ea68532f6383a048daac380765c1f6e20a9b5e3d60961db4e2b&query=chicken')
-        print data.to_json
+        puts data
         data
     end
 
     get "/data" do 
         {"test": "string"}.to_json
     end    
+
+    get "/scrape" do
+        doc = Nokogiri::HTML(open('http://www.nokogiri.org/tutorials/installing_nokogiri.html'))
+        "hello"
+        puts doc
+    end
 end
 
--- breakouts/sinatra/app/config.ru
+++ breakouts/sinatra/app/config.ru
@@ -1,3 +1,6 @@
 require "./app"
 
+
+# set :database, {adapter: "mysql2", host: "localhost", port: 3306, database: "cat_db", username: "root", password: "password" }  
+
 run HiSinatra
