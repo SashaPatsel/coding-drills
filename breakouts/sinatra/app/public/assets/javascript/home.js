457e563d6b8307dab2da8d21c9742a42f6894af1
Sasha Patsel <user@calvisitor-10-105-129-184.calvisitor.1918.berkeley.edu>
2019-01-23 10:40:33 -0800

    dynamically generate n amount of rows for saved movies

--- breakouts/sinatra/app/public/assets/javascript/home.js
+++ breakouts/sinatra/app/public/assets/javascript/home.js
@@ -19,11 +19,13 @@ function displayAllSaved() {
         $("#savedMovies").empty()
         for (var i = 0 ; i < data.length ; i++) {
          $("#savedMovies").append(`
-            <div class="card" style="width: 18rem;">
-            <img src="${data[i].movie_poster}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${data[i].movie_name}</h5>
-                    <p class="card-text">${data[i].movie_year}</p>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                <img src="${data[i].movie_poster}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${data[i].movie_name}</h5>
+                        <p class="card-text">${data[i].movie_year}</p>
+                    </div>
                 </div>
             </div>
             `)
